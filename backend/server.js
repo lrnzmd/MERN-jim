1e20f0323b24630b8433876a9a9a2e1990497679
Amodeo Lorenzo <lorenzo@solutions.io>
2022-07-31 13:53:14 +0200

    end workout controllers

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 const mongoose = require("mongoose");
 
-const workputRouters = require("./routes/workout");
+const workputRouters = require("./routes/workouts");
 
 const app = express();
 
