69ca93edeebb32521264146c4cca334d487a2acb
Amodeo Lorenzo <lorenzo@solutions.io>
2022-08-21 23:17:53 +0200

    lesson11

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,11 @@
 import { useState, useEffect} from 'react'
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 const Home = () =>{ 
 
-    const [workouts, setWorkouts] = useState(null)
+  const { workouts, dispatch } = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -12,7 +13,7 @@ const Home = () =>{
           const json = await response.json()
     
           if (response.ok) {
-            setWorkouts(json)
+            dispatch({type: 'SET_WORKOUTS', payload: json})
           }
         }
     
@@ -28,8 +29,8 @@ const Home = () =>{
           {workouts && workouts.map(workout => (
             <WorkoutDetails key={workout._id} workout={workout}/>
           ))}
-          <WorkoutForm/>
         </div>
+          <WorkoutForm/>
       </div>
     )
 }
