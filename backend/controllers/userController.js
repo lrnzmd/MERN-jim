6803a5692bcd2e5daf08e6fc1a87a5e7d221f778
Lorenzo Amodeo <lrnzmd@gmail.com>
2022-08-28 12:33:40 +0200

    lesson 17 token singup + fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // login useer
 const loginUser = async (req, res) => {
@@ -10,9 +15,14 @@ const singupUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.singup(email, password);
+    const user = await User.singup(email, password)
 
-    res.status(200).json([email, user]);
+    // create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, token})
+
+
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
